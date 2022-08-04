1237e28465373802c8207b28679a0c887ee13f87
castillojuliusdave <julius.dave.castillo@prulifeuk.com.ph>
2022-08-05 00:36:40 +0800

    push changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
+import friend from './assets/friend.png';
+import ohHi from './assets/ohHi.mp3';
 import './App.css';
+let clickCounter = 0;
+let clickCountGlobal = 0;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={friend} className="App-logo" alt="logo" />
+        <div id="global">{clickCountGlobal}</div>
+        <div id="local">{clickCounter}</div>
+        <div className="button-wrapper">
+        <audio id="audio" src={ohHi}></audio>
+          <button id="im-pomu" 
+            type="button" 
+            onClick={() => {
+              var audio = document.getElementById("audio");
+              audio.play();
+              console.log("hi");
+              if (typeof(Storage) !== "undefined") {
+                if (localStorage.clickcount1) {
+                  localStorage.clickcount1 = Number(localStorage.clickcount1)+1;
+                  clickCounter = clickCounter+1
+                } else {
+                  clickCounter = 1;
+                  localStorage.clickcount1 = 1;
+                }
+                clickCountGlobal = localStorage.clickcount1;
+                document.getElementById("global").innerHTML = clickCountGlobal;
+                document.getElementById("local").innerHTML = clickCounter;
+                console.log('clickCountGlobal--', clickCountGlobal);
+                console.log('clickCounter--', clickCounter);
+              } else {
+                clickCounter = "Sorry, your browser does not support web storage...";
+              }
+            }}>
+            oh Hi!
+          </button> 
+        </div>
       </header>
     </div>
   );
